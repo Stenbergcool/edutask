1ac889579c7a5c36d09a9d2df73e936fbdd74849
Eric Samuelsson <gossen02@gmail.com>
2023-04-17 16:57:10 +0100

    Unit test done for assignment 2

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,35 +1,67 @@
 import pytest
-from src.controllers.usercontroller import UserController
 import unittest.mock as mock
+from src.controllers.usercontroller import UserController
+
+"""Email exists, email valid and email not unique"""
+def test_valid_email_exist_not_unique():
+    mockedDao = mock.MagicMock()
+    mockedDao.find.return_value = [{'Email': 'hello@gmail.com'}, {'Email': 'hello@gmail.com'}]
+    user_controller = UserController(mockedDao)
+    result = user_controller.get_user_by_email('hello@gmail.com')
+    assert result == {'Email': 'hello@gmail.com'}
+
+"""Email exists, email not valid and email unique"""
+def test_not_valid_exist_unique():
+    with pytest.raises(ValueError, match='Error: invalid email address'):
+        mockedDao = mock.MagicMock()
+        mockedDao.find.return_value = [{'Email': 'hello'}]
+        user_controller = UserController(mockedDao)
+        user_controller.get_user_by_email('hello')
+
+"""Email exists, email valid and email unique"""
+def test_valid_exist_unique():
+    mockedDao = mock.MagicMock()
+    mockedDao.find.return_value = [{'Email': 'hello@gmail.com'}]
+    user_controller = UserController(mockedDao)
+    result = user_controller.get_user_by_email('hello@gmail.com')
+    assert result == {'Email': 'hello@gmail.com'}
+
+"""Email exists, email not valid and email not unique"""
+def test_not_valid_exist_not_unique():
+    with pytest.raises(ValueError, match='Error: invalid email address'):
+        mockedDao = mock.MagicMock()
+        mockedDao.find.return_value = [{'Email': 'hello'}]
+        user_controller = UserController(mockedDao)
+        user_controller.get_user_by_email('hello')
 
 """Email does not exists, email valid and email unique"""
 def test_valid_email_does_not_exist_unique():
-    mockeddao = mock.MagicMock()
-    mockeddao.find.return_value = None
-    user_contoller = UserController(mockeddao)
-    result = user_contoller.get_user_by_email("hello@gmail.com")
+    mockedDao = mock.MagicMock()
+    mockedDao.find.return_value = None
+    user_controller = UserController(mockedDao)
+    result = user_controller.get_user_by_email("hello@gmail.com")
     assert result == None
 
 """Email does not exists, email valid and email not unique"""
-def test_valid_email_does_not_exit_not_unique():
-    mockeddao = mock.MagicMock()
-    mockeddao.find.return_value = None
-    user_contoller = UserController(mockeddao)
-    result = user_contoller.get_user_by_email("hello@gmail.com")
+def test_valid_email_does_not_exist_not_unique():
+    mockedDao = mock.MagicMock()
+    mockedDao.find.return_value = None
+    user_controller = UserController(mockedDao)
+    result = user_controller.get_user_by_email("hello@gmail.com")
     assert result == None
 
 """Email does not exists, email not valid and email unique"""
-def test_not_valid_email_does_not_exit():
+def test_not_valid_email_does_not_exist():
     with pytest.raises(ValueError, match=r"Error: invalid email address"):
-        mockeddao = mock.MagicMock()
-        mockeddao.find.return_value = None
-        user_contoller = UserController(mockeddao)
-        user_contoller.get_user_by_email("hello")
+        mockedDao = mock.MagicMock()
+        mockedDao.find.return_value = None
+        user_controller = UserController(mockedDao)
+        user_controller.get_user_by_email("hello")
 
 """Email does not exists, email not valid and email not unique"""
-def test_not_valid_email_does_not_exit_not_unique():
+def test_not_valid_email_does_not_exist_not_unique():
     with pytest.raises(ValueError, match=r"Error: invalid email address"):
-        mockeddao = mock.MagicMock()
-        mockeddao.find.return_value = None
-        user_contoller = UserController(mockeddao)
-        user_contoller.get_user_by_email("hello")
+        mockedDao = mock.MagicMock()
+        mockedDao.find.return_value = None
+        user_controller = UserController(mockedDao)
+        user_controller.get_user_by_email("hello")
